4bf97f1df3b106395d1235afee73a745c357643b
Pascal P <67502663+PascalP1998@users.noreply.github.com>
2024-02-24 13:11:18 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
     );
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, credentials"
     );
     next();
 });
